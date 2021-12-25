40e3ac681cef42349bfb29bee8157e18d495266a
Ramen Manna <ramen209admit@gmail.com>
2021-12-25 14:45:51 +0530

    first commit by ramen

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is basic Todo List React App
+
---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h5>Hi I am Ramen Manna. Beginner to Learn React Js.</h5>
+            <p>Thanks for visiting My React Site.</p>
+        </div>
+    )
+}
